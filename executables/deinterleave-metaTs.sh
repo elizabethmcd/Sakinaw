c6a2d6b905b9d56fce1a4dcd20d070557d25da36
Elizabeth McDaniel <emcdaniel@wisc.edu>
2022-03-15 17:00:07 -0700

    deinterleave and fix kallisto mapping

---
+++ executables/deinterleave-metaTs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=def-shallam
+#SBATCH --array=1-19
+#SBATCH --time 12:0:0
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=2G
+#SBATCH --job-name=deinterleave-metaTs
+#SBATCH --output=deinterleave-metaTs.out
+#SBATCH --mail-user=eamcdani@mail.ubc.ca
+#SBATCH --mail-type=ALL
+
+# array job
+metaT_file=$(sed -n "${metatranscriptome-sample-codes.txt}p" sakinaw_metagenomes.txt | awk -F "\t" '{print $2}')
+metaT_name=$(basename $metaT_file .fastq)
+
+# paths 
+project_path="/home/eamcdani/projects/def-shallam/eamcdani/sakinaw"
+metaT_path="${project_path}/metaT_mapping/fastqs/${metaT_file}"
+out_path="${project_path}/metaT_mapping/deinterleaved"
+
+# load module
+module load bbmap 
+
+# reformat from interleaved to deinterleaved
+reformat.sh in=$metaT_path out1=${out_path}/${metaT_name}_1.fastq out2=${out_path}/${metaT_name}_2.fastq
--- executables/kallisto-mapping-queue.sh
+++ executables/kallisto-mapping-queue.sh
@@ -13,16 +13,18 @@
 sample_name=$(sed -n "${metatranscriptome-sample-codes.txt}p" sakinaw_metagenomes.txt | awk -F "\t" '{print $3}')
 metaT_file=$(sed -n "${metatranscriptome-sample-codes.txt}p" sakinaw_metagenomes.txt | awk -F "\t" '{print $2}')
 
+metaT_name=$(basename $metaT_file .fastq)
+
 # paths 
 project_path="/home/eamcdani/projects/def-shallam/eamcdani/sakinaw"
-index_file="${project_path}/metaT_mapping/all-sakinaw-orfs.ffn"
-metaT_path="${project_path}/metaT_mapping/fastqs/${metaT_file}"
+index_file="${project_path}/metaT_mapping/sakinaw-orfs"
+metaT_path="${project_path}/metaT_mapping/deinterleaved"
 out_dir="${project_path}/metaT_mapping/kallisto_output/${sample_name}"
 
 # load modules
 module load kallisto
 
 # kallisto quant command
-kallisto quant -i $index_file -o $out_dir $metaT_path
+kallisto quant -i $index_file -o $out_dir ${metaT_path}/${metaT_name}_1.fastq ${metaT_path}/${metaT_name}_2.fastq
 
 

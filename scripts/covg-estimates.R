5bb68935104a8565f8528626981dc1d3ac6c5128
Elizabeth McDaniel <emcdaniel@wisc.edu>
2022-03-17 17:10:55 -0700

    rel abund and covg

---
+++ scripts/covg-estimates.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+########################################
+# Preliminary metawrap bins 
+# Coverage and breadth stats for each bin in each sample to determine if have enough coverage for downstream diversity analyses 
+########################################
+
+coverage_info_path <- "results/metawrap_bins/inStrain/covg/"
+files <- dir(coverage_info_path, pattern="*.csv")
+all_coverage <- data_frame(filename = files) %>% 
+  mutate(file_contents = map(filename, ~ read.csv(file.path(coverage_info_path, .)))
+         ) %>% 
+  unnest()
+
+coverage_filtered_table <- all_coverage %>% 
+  mutate(sample = gsub("-vs-bins.csv", "", filename)) %>% 
+  mutate(sample = gsub("_metaG", "", sample)) %>% 
+  mutate(Genome = genome) %>% 
+  filter(coverage > 10 & breadth > 0.9) %>% 
+  select(Genome, sample, coverage, breadth) %>% 
+  left_join(final_bins_table)
+
+covg_counts <- coverage_filtered_table %>% 
+  group_by(Genome, classification) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n))
---
+++ scripts/relative_abundance.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(matrixStats)
+
+########################################
+# Preliminary metawrap bins 
+# Relative abundance by mapping back metagenomic reads to the dereplicated set of genomes
+########################################
+
+relative_abundance <- read_tsv("results/metawrap_bins/sakinaw_rel_abund_table.tsv")
+colnames(relative_abundance) <- gsub("_metaG-vs-bins.sorted.*", "", colnames(relative_abundance))
+colnames(relative_abundance) <- gsub("-vs-bins.sorted.*", "", colnames(relative_abundance))
+
+rel_abund_stats <- relative_abundance %>% 
+  filter(Genome != "unmapped") %>% 
+  pivot_longer(!Genome, names_to="sample", values_to="relative_abundance") %>% 
+  group_by(Genome) %>% 
+  summarise(mean = mean(relative_abundance), max=max(relative_abundance), min=min(relative_abundance), median=median(relative_abundance)) %>% 
+  arrange(desc(max))
+
+colnames(final_bins_table)[1] <- c("Genome")
+  
+rel_abund_info_table <- left_join(rel_abund_stats, final_bins_table)
+
+rel_abund_long <- relative_abundance %>% 
+  filter(Genome != "unmapped") %>% 
+  pivot_longer(!Genome, names_to="sample", values_to="relative_abundance") %>% 
+  left_join(final_bins_table)
+
+rel_abund_long %>% 
+  ggplot(aes(x=Genome, y=relative_abundance)) +
+  geom_bar(aes(fill=phylum), stat="identity") +
+  facet_wrap(~ sample)
+
+rel_abund_long %>% 
+  extract(sample, into = c("date", "depth"), "(.*)_([^_]+)$") %>% 
+  mutate(date = gsub("sak_", "", date)) %>% 
+  filter(depth != "May2011") %>% 
+  ggplot(aes(x=relative_abundance, y=(depth))) +
+  geom_point(aes(color=Genome)) +
+  facet_wrap(~ date) + 
+  theme(legend.position="none")
+
+  
